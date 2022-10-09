763b5c606de3ee198d3e09c94d7e5603108c879a
Emilia Bartic <48402982+EmiliaBartic@users.noreply.github.com>
2022-10-09 15:29:09 +0300

    Update ex1.py

--- Laborator2/ex1.py
+++ Laborator2/ex1.py
@@ -7,23 +7,28 @@ import arviz as az
 import random
 import statistics
 #x = stats.expon.rvs(0, 1, 10000)
+#lambda1=4 hrs^-1 => p1=1/4=0.25
+p_lambda_1 = 0.25
 
-p1 = 0.25
-p2 = 0.16
+#lamda2=6 hrs^-1 => p2=1/6=0.16
+p_lambda_2 = 0.16
+
 X = []
-for n in range(1, 10000):
+
+#generati 10.000 de valori pentru X
+for n in range(1, 10_000):
     x = random.randint(1, 100)
     if x < 40:
         # mecanicul 1
-        x_m1 = stats.expon.rvs(0, p1, 1)
-        X.append(x_m1[0])
+        x_mecanic1 = stats.expon.rvs(0, p_lambda_1, 1)
+        X.append(x_mecanic1[0])
     else:
         # mecanicul 2
-        x_m2 = stats.expon.rvs(0, p2, 1)
-        X.append(x_m2[0])
+        x_mecanic2 = stats.expon.rvs(0, p_lambda_2, 1)
+        X.append(x_mecanic2[0])
 
 az.plot_posterior({'x': X})
-plt.show()
+plt.show() #afisarea graficului densitatii distributiei lui X
 # media ca numar
 print(statistics.mean(X))
 # deviatia standard
